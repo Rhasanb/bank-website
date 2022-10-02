35838fdc0b9c16e305ba2b69c04ecca8e87cf901
Rakib hasan <rakibhasab91@gmail.com>
2022-10-02 19:40:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const loginBtn = document.getElementById("login");
+// const loginArea = document.getElementById("login-area");
+// const transactionArea = document.getElementById("transaction-area");
+// loginBtn.addEventListener("click", function(){
+
+// loginArea.style.display ="none";
+// transactionArea.style.display = "block";  
+// });
